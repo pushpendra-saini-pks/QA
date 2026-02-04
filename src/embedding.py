3078822305e7cf6e39a48ef90bf617ef85f17a62
pushpendra-saini-pks <pk.karauli2000@gmail.com>
2026-02-04 19:57:05 +0530

    icoomit

--- src/embedding.py
+++ src/embedding.py
@@ -1,4 +1,4 @@
-from langchain_pinecone.vectorstores import PineconeVectorStore
+from langchain_pinecone.vectorstores import Pinecone
 # from langchain.docstore.document import Document 
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 # initialize the embeddings model 
@@ -11,5 +11,5 @@ doc_embeddings = GoogleGenerativeAIEmbeddings(
 index_name = "test1"
 
 def store_embeddings(document_chunks):
-    docsearch = PineconeVectorStore.from_documents(document_chunks,doc_embeddings,index_name=index_name)
+    docsearch = Pinecone.from_documents(document_chunks,doc_embeddings,index_name=index_name)
     return docsearch
